75ee7ed3d333e1020a3e73b907c9d667890ecfbc
Fran <hi@frangrgic.com>
2026-04-06 11:56:20 +0200

    fix: nav bar disappearing (#90)

--- apps/website/src/components/nav-bar/nav-bar.tsx
+++ apps/website/src/components/nav-bar/nav-bar.tsx
@@ -73,7 +73,7 @@ export function NavBar() {
 	return (
 		<header
 			className={[
-				'sticky top-0 z-50 transition-all duration-300',
+				'fixed top-0 left-0 right-0 z-50 transition-all duration-300',
 				scrolled ? 'px-4 pt-2' : 'px-0 pt-0',
 			].join(' ')}
 		>
