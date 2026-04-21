9d74523b77410eaaa655112b1a08a4d78f2f6af4
Fran Grgić <grgicfran@gmail.com>
2026-04-21 02:20:33 +0200

    feat: export presets entry

--- packages/hextimator/tsup.config.ts
+++ packages/hextimator/tsup.config.ts
@@ -5,6 +5,7 @@ export default defineConfig([
 		entry: {
 			index: 'src/index.ts',
 			react: 'src/react/index.ts',
+			presets: 'src/presets/index.ts',
 		},
 		external: ['react'],
 		format: ['esm'],
